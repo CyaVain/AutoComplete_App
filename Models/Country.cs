9e8f9cbb9ec619e175c4c9d3cef1be1a464ca186
Kevin Octavianus Halim <vainezz121@gmail.com>
2024-04-18 15:26:40 +0700

    First Init

---
+++ Models/Country.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AutoComplete_App.Models
+{
+    public class Country
+    {
+        private string name;
+        private string code;
+
+        [JsonPropertyName("name")]
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        [JsonPropertyName("code")]
+        public string Code
+        {
+            get { return code; }
+            set { code = value; }
+        }
+    }
+}
---
+++ Services/CallApi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using AutoComplete_App.Models;
+
+namespace AutoComplete_App.Services
+{
+    public class CallApi
+    {
+        readonly HttpClient client;
+        
+        static string fileOutput = @"..\\AutoComplete_App\\countries.json";
+
+        public Country[] Countries { get; private set; }
+
+        public CallApi(HttpClient httpClient)
+        {
+            client = httpClient;
+        }
+
+        public async Task GetCountryList()
+        {
+            string url = "https://gist.githubusercontent.com/almost/7748738/raw/575f851d945e2a9e6859fb2308e95a3697bea115/countries.json";
+            var response = await client.GetStringAsync(url);
+
+            File.WriteAllText(fileOutput, response);
+
+            await ReadCountry();
+        }
+
+        public async Task ReadCountry()
+        {
+            string json = File.ReadAllText(fileOutput);
+            Countries = JsonSerializer.Deserialize<Country[]>(json);
+        }
+    }
+}
