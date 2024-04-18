7a1673d58b28fef521924553784710a236a96693
Kevin Octavianus Halim <vainezz121@gmail.com>
2024-04-18 16:48:17 +0700

    AutoComplete Ok

--- obj/Release/net8.0/AutoComplete_App.AssemblyInfo.cs
+++ obj/Release/net8.0/AutoComplete_App.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoComplete_App")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e8f9cbb9ec619e175c4c9d3cef1be1a464ca186")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoComplete_App")]
